0fea8b8d5b3361228d21e60535b7403522e41673
Jaden <dev.zerob@gmail.com>
2022-03-25 10:56:40 +0900

    Add logic to find a pair
    returns count of the most common card from array
    returns just the values from a hand

--- __tests__/card/check-hand-test.js
+++ __tests__/card/check-hand-test.js
@@ -1,5 +1,36 @@
-const checkHand = require('../../card/check-hand.js');
+const card = require('../../card/check-hand.js');
 
-test('one pair', () => {
-  expect(checkHand(['2-H', '3-C', '4-D', '5-H', '2-C'])).toBe('pair');
+describe('checkHand()', () => {
+  it('handles pairs', () => {
+    expect(card.checkHand(['2-H', '3-C', '4-D', '5-H', '2-C'])).toBe('pair');
+    expect(card.checkHand(['3-H', '3-C', '4-D', '5-H', '2-C'])).toBe('pair');
+  });
+
+  it('handles three of a kind', () => {
+    expect(card.checkHand(['3-H', '3-C', '3-D', '5-H', '2-H'])).toBe('three of a kind');
+  });
+});
+
+describe('isPair()', () => {
+  it('finds a pair', () => {
+    expect(card.isPair(['2-H', '3-C', '4-D', '5-H', '2-C'])).toBeTruthy();
+  });
+});
+
+describe('multiplesIn()', () => {
+  it('finds a duplicate', () => {
+    expect(card.multiplesIn(['2-H', '3-C', '4-D', '5-H', '2-C'])).toBe(2);
+  });
+});
+
+describe('valuesFromHand()', () => {
+  it('returns just the values from a hand', () => {
+    expect(card.valuesFromHand(['2-H', '3-C', '4-D', '5-H', '2-C'])).toEqual(['2', '3', '4', '5', '2']);
+  });
+});
+
+describe('highestCount()', () => {
+  it('returns count of the most common card from array', () => {
+    expect(card.highestCount(['2', '4', '4', '4', '2'])).toBe(3);
+  });
 });
--- card/check-hand.js
+++ card/check-hand.js
@@ -1,3 +1,58 @@
-const checkHand = function(){};
+// Check the result of the cards in hand.
+const checkHand = function(hand){
+  if (isPair(hand)) {
+    return 'pair';
+  } else {
+    return 'three of a kind'
+  }
+};
 
-module.exports = checkHand;
+// Finds a pair
+const isPair = function(hand) {
+  return multiplesIn(hand) === 2;
+}
+
+// Finds a duplicate
+const multiplesIn = function(hand) {
+  return highestCount(valuesFromHand(hand));
+}
+
+// Returns count of the most common card from array
+const highestCount = function(values) {
+  const counts = {};
+  values.forEach((value) => {
+    counts[value] = 0;
+    if (value === values[0]) {
+      counts[value] = counts[value] + 1;
+    }
+    if (value === values[1]) {
+      counts[value] = counts[value] + 1;
+    }
+    if (value === values[2]) {
+      counts[value] = counts[value] + 1;
+    }
+    if (value === values[3]) {
+      counts[value] = counts[value] + 1;
+    }
+    if (value === values[4]) {
+      counts[value] = counts[value] + 1;
+    }
+  });
+  const totalCounts = Object.keys(counts).map((key) => counts[key]);
+  return totalCounts.sort((a, b) => b-a)[0];
+}
+
+// Returns just the values from a hand
+const valuesFromHand = function(hand) {
+  return hand.map((card) => {
+    return card.split('-')[0];
+  });
+}
+
+module.exports = {
+  checkHand: checkHand,
+  isPair: isPair,
+  multiplesIn: multiplesIn,
+  highestCount: highestCount,
+  valuesFromHand: valuesFromHand
+};
